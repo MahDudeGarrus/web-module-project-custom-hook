f221f3f39dcf725681ad2ba116769c5d8f7b8539
Stephanie Ann Solis <stephaniesolis@lambdastudents.com>
2021-07-14 07:22:02 -0600

    change in dark mode hook

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,7 +1,7 @@
 import { useLocalStorage } from "./useLocalStorage";
 
 export const useDarkMode = key => {
-    const [darkMode, setDarkMode] = useLocalStorage( 'dark mode', key);
+    const [darkMode, setDarkMode] = useLocalStorage(key);
 
     return [darkMode, setDarkMode];
 }
